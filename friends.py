86d6999f85534cee3b500ca08fa061062b23ca81
Zainab Khan <zainabk@nvidia.com>
2019-02-04 00:16:00 -0800

    Commiting files

---
+++ friends.py
@@ -0,0 +1,40 @@
+users =[
+    { "id":0, "name": "Hero" },
+    { "id":1, "name": "Dunn" },
+    { "id":2, "name": "Sue" },
+    { "id":3, "name": "Chi" },
+    { "id":4, "name": "Thor" },
+    { "id":5, "name": "Clive" },
+    { "id":6, "name": "Hicks" },
+    { "id":7, "name": "Devin" },
+    { "id":8, "name": "Kate" },
+    { "id":9, "name": "Klein" }    
+]
+
+friendship = [
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (6, 8),
+    (7, 8),
+    (8, 9)
+]
+
+def num_friends(user):
+    '''
+    Find number of friends for a given user
+    '''
+    t = [f for _friendship in friendship for f in _friendship]
+    return t.count(user["id"])
+              
+def sort_by_num_friends():
+    counts = {u["name"]: num_friends(u) for u in users}
+    for p in sorted(counts.items(), key=lambda item: item[1]):
+        print("{person} has {num} number of friends".format(person=p[0], num=p[1]))
+sort_by_num_friends()
